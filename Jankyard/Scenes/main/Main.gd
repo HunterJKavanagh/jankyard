596ead49dcefe95186f8e2f56b27889875231f7a
HunterJKavanagh <42754269+HunterJKavanagh@users.noreply.github.com>
2020-02-01 13:19:35 -0600

    Dialog V1

--- Jankyard/Scenes/main/Main.gd
+++ Jankyard/Scenes/main/Main.gd
@@ -15,3 +15,10 @@ func on_level_change(dir):
 			$Player.position = $Map.player_postions[lib.DIR.up]
 		lib.DIR.left:
 			$Player.position = $Map.player_postions[lib.DIR.right]
+
+func _on_Character_input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton:
+		if event.pressed and event.button_index == BUTTON_LEFT:
+			var dis = sqrt(pow($Player.position.x - $Character.position.x, 2) + pow($Player.position.y - $Character.position.y, 2))
+			if dis <= 100:
+				$UI/P2/TextBox.text += "Don't Click Me!"
