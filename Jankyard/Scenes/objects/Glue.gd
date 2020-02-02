11c4db4f43b9273377cb5eb26666fe4ade423a0b
HunterJKavanagh <42754269+HunterJKavanagh@users.noreply.github.com>
2020-02-02 13:08:35 -0600

    idk

--- Jankyard/Scenes/objects/Glue.gd
+++ Jankyard/Scenes/objects/Glue.gd
@@ -12,7 +12,6 @@ func _on_Glue_input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton:
 		if event.pressed and event.button_index == BUTTON_LEFT:
 			emit_signal("obj_clicked", self.get_meta("obj"), lib.LEFT)
-			if $"./Player".selected_tool == lib.TOOLS.hands:
-				self.queue_free()
+			self.queue_free()
 		if event.pressed and event.button_index == BUTTON_RIGHT:
 			emit_signal("obj_clicked", self.get_meta("obj"), lib.RIGHT)
