77383954f9cf0540fd349f749088ca8f7923ebb6
Bigguburaun <guypenguin4@gmail.com>
2020-02-02 11:00:55 -0600

    Added F3-3L

--- Jankyard/Scenes/characters/F3-3L/F3-3L.gd
+++ Jankyard/Scenes/characters/F3-3L/F3-3L.gd
@@ -3,8 +3,8 @@ extends KinematicBody2D
 const lib = preload("res://Lib.gd")
 
 func _ready():
-	self.set_meta("character", lib.CHARACTERS._3V_1L)
+	self.set_meta("character", lib.CHARACTERS.F3_3L)
 	self.set_meta("level_postions", {
-		lib.LEVELS.hub: Vector2(800,100),
+		lib.LEVELS.hub: Vector2(900,300),
 		lib.LEVELS.level_2: Vector2(200,100),
 	})
